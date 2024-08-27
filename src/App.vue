297fc602ea959d0904f7d89ffefb8d6503ece538
Gerry <gerrydesrian488@gmail.com>
2024-08-27 07:36:11 +0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+      <header id="title">
+        <h1 id="title">QuizVue</h1>
+        <input type="text" id="search-input">
+      </header>
+    </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 900px; 
+  margin: 0 auto;
+}
+
+header {
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#title {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+#search-input {
+  border: none;
+  background-color: #c9c9c9a9;
+  padding: 10px;
+  border-radius: 5px;
+}
+</style>
