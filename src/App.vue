0686fffc56c9f89502d532ac4cb2261b08fdd872
Gerry <gerrydesrian488@gmail.com>
2024-08-27 08:10:58 +0700

    component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, watch } from 'vue'
 import srcQuiz from './data/quizes.json'
+import QuizCard from './components/QuizCard.vue';
 
 const quizez = ref(srcQuiz)
 const search = ref('')
@@ -19,14 +20,7 @@ watch(search, () => {
         <input v-model.trim="search" type="text" id="search-input">
       </header>
       <section id="quiz-container">
-        <div v-for="quiz in quizez" :key="quiz.id" class="card">
-          <img :src="quiz.img" 
-               :alt="quiz.title">
-          <div class="card-body">
-            <h2>{{ quiz.title }}</h2>
-            <p>{{ quiz.questions.length }} Questions</p>
-          </div>
-        </div>
+        <QuizCard :quizez="quizez"></QuizCard>
       </section>
     </main>
 </template>
@@ -60,30 +54,6 @@ header {
   display: flex;
   flex-wrap: wrap;
   margin-top: 20px;
-}
-
-.card {
-  width: 270px;
-  margin-right: 30px;
-  margin-bottom: 30px;
-  border-radius: 5px;
-  overflow: hidden;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-}
-
-.card img {
-  width: 100%;
-  height: 200px;
-  object-fit: cover;
-  margin: 0;
-}
-
-.card-body {
-  padding: 0 15px;
-}
-
-.card-body h2 {
-  font-weight: bold;
 }
 
 </style>
