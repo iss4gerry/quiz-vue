4bba3cc4ebc241b163cbabffcfcc17f81dc0368e
gerry <gerrydesrian488@gmail.com>
2024-08-28 11:06:06 +0700

    page number

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,7 +2,7 @@
 import QuizHeader from '@/components/QuizHeader.vue'
 import QuizSection from '@/components/QuizSection.vue'
 import { useRoute } from 'vue-router';
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import quizes from '../data/quizes.json'
 
 const route = useRoute()
@@ -10,12 +10,20 @@ const quizId = parseInt(route.params.id)
 const quiz = quizes.find((q) => q.id === quizId)
 
 const currentQuestionIndex = ref(0)
+const questionPage = ref(`${currentQuestionIndex.value + 1} / ${ quiz.questions.length }`)
+
+watch(() => currentQuestionIndex.value,
+      () => {
+        questionPage.value = `${currentQuestionIndex.value + 1} / ${ quiz.questions.length }`
+      }
+)
 
 </script>
 
 <template>
-    <QuizHeader></QuizHeader>
+    <QuizHeader :questionPage="questionPage"></QuizHeader>
     <QuizSection :question="quiz.questions[currentQuestionIndex]"></QuizSection>
-    
-</template>
+    <button @click="currentQuestionIndex++" 
+            :disabled="currentQuestionIndex === quiz.questions.length - 1">Next</button>
+</template> 
 
